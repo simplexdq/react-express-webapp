ea46f163d58fdcebfad025c0725e4840bda3fa99
Dongqi Xu <dongqixu@DongqideMacBook-Pro.local>
2020-08-22 23:30:34 -0400

    Third Commit

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -3,7 +3,6 @@ import { Link, withRouter } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { createProfile } from '../../actions/profile'
-import { createStore } from 'redux'
 
 const CreateProfile = ({ createProfile, history }) => {
   const [formData, setFormData] = useState({
--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -46,7 +46,7 @@ const EditProfile = ({
       youtube: loading || !profile.social ? '' : profile.social.youtube,
       instagram: loading || !profile.social ? '' : profile.social.instagram,
     })
-  }, [loading]) //change only when loading changes useEffect性能优化
+  }, [loading, getCurrentProfile]) //change only when loading changes useEffect性能优化
 
   const {
     company,
